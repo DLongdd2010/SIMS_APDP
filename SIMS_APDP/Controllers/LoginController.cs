25a07abe28d8ce051fd3a22cb2e79701b6ba731e
DLongdd2010 <daoduylong20102005@gmail.com>
2025-12-03 13:12:38 +0700

    first commit

---
+++ SIMS_APDP/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIMS_APDP.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
