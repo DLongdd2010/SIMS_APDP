dbb609ff2dd4c1b97807df35b731064bb13f975a
DLongdd2010 <daoduylong20102005@gmail.com>
2025-12-06 22:35:15 +0700

    long uploa timetable

--- SIMS_APDP/Models/Course.cs
+++ SIMS_APDP/Models/Course.cs
@@ -11,9 +11,11 @@ namespace SIMS_APDP.Models
         public string CourseName { get; set; }
         public string? Description { get; set; }
         [Required, Range(1, 10)]
-        public int Credits { get; set; }
+        public byte Credits { get; set; }
         public int? TeacherId { get; set; }// Thêm TeacherId để gán teacher cho course
         [ForeignKey("TeacherId")]
         public virtual User? Teacher { get; set; }// Navigation property
+
+        public ICollection<StudentCourse> StudentCourses { get; set; } = new List<StudentCourse>();
     }
 }
